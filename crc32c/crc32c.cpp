e69b41a5d3b60d03a4c419304664a6dbc18a459d
Robert Vazan <robvcode@gmail.com>
2014-02-10 18:10:47 +0100

    License notice

--- crc32c/crc32c.cpp
+++ crc32c/crc32c.cpp
@@ -1,5 +1,27 @@
-// crc32c.cpp : Defines the exported functions for the DLL application.
-//
+/*
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the author be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Mark Adler
+  madler@alumni.caltech.edu
+
+  Note by Robert Vazan:
+  The source was modified for compatibility with Visual C++ and to run in 32-bit mode.
+  I've added benchmark & test code and a trivial implementation for consistency checks.
+ */
 
 #include "stdafx.h"
 #include "crc32c.h"
