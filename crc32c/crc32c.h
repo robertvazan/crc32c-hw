c20f4f0bb8ca2838b6927c0a199481b7bf92f675
Robert Vazan <robvcode@gmail.com>
2014-02-14 23:04:17 +0100

    Updated comments

--- crc32c/crc32c.h
+++ crc32c/crc32c.h
@@ -14,15 +14,15 @@
 #include <stdint.h>
 
 /*
-    Computes CRC-32C using Castagnoli polynomial of 0x82f63b78.
-    This polynomial is better at detecting errors than the more common CRC-32 polynomial.
-    CRC-32C is implemented in hardware on newer Intel processors.
-    This function will use the hardware if available and fall back to fast software implementation.
+    Computes CRC-32C (Castagnoli) checksum. Uses Intel's CRC32 instruction if it is available.
+    Otherwise it uses a very fast software fallback.
 */
 extern "C" CRC32C_API uint32_t crc32c_append(
-    uint32_t crc,               // initial CRC, typically 0, may be used to accumulate CRC from multiple buffers
-    const uint8_t *input,       // data to be put through the CRC algorithm
-    size_t length);             // length of the data in the input buffer
+    uint32_t crc,               // Initial CRC value. Typically it's 0.
+                                // You can supply non-trivial initial value here.
+                                // Initial value can be used to chain CRC from multiple buffers.
+    const uint8_t *input,       // Data to be put through the CRC algorithm.
+    size_t length);             // Length of the data in the input buffer.
 
 extern "C" CRC32C_API void crc32c_unittest();
 
